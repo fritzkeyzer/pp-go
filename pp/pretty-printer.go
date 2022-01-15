01e953c75cd1171179d56754ded4dd94b0cd56e3
fritzkeyzer <fritzkeyzer@gmail.com>
2022-01-15 20:22:14 +0200

    Initial commit

---
+++ pp/pretty-printer.go
@@ -0,0 +1,85 @@
+package pp
+
+// https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences?redirectedfrom=MSDN
+
+// https://en.wikipedia.org/wiki/ANSI_escape_code
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"reflect"
+	"strings"
+)
+
+var (
+	a = color.New(color.FgGreen, color.Bold)
+	b = color.New(color.FgRed, color.Bold)
+)
+
+func Dump(s interface{}) {
+
+	title := reflect.ValueOf(s).Type().String()
+
+	var fieldNames []string
+	var fieldTypes []string
+	var fieldVals []string
+
+	sP := reflect.ValueOf(s).Elem()
+	for i := 0; i < sP.NumField(); i++ {
+		fieldName := sP.Type().Field(i).Name
+		fieldType := fmt.Sprintf("%v", sP.Type().Field(i).Type)
+		fieldVal := fmt.Sprintf("%v", sP.Field(i))
+
+		fieldNames = append(fieldNames, fieldName)
+		fieldTypes = append(fieldTypes, fieldType)
+		fieldVals = append(fieldVals, fieldVal)
+	}
+
+	lnName := 0
+	lnVal := 0
+	for i, _ := range fieldNames {
+		if len(fieldNames[i]) > lnName {
+			lnName = len(fieldNames[i])
+		}
+		if len(fieldVals[i]) > lnVal {
+			lnVal = len(fieldVals[i])
+		}
+	}
+
+	a.Printf("%s\n", "- - - - - - - - - - - - - - - - - - ")
+	a.Printf("%s\n", title)
+
+	for i, _ := range fieldNames {
+		name := fmt.Sprintf("%s", fieldNames[i])
+		val := fmt.Sprintf("%-*s", lnVal, fieldVals[i])
+		typ := fmt.Sprintf("%s", fieldTypes[i])
+
+		padName := lnName - len(fieldNames[i])
+		padVal := lnVal - len(fieldVals[i])
+
+		a.Printf("\t%s: ", padLeft(name, " ", padName))
+		color.Unset()
+		b.Printf("%s ", padRight(val, "", padVal))
+		color.Unset()
+		a.Printf("\t%s\n", typ)
+	}
+	a.Printf("%s\n", "- - - - - - - - - - - - - - - - - - ")
+}
+
+func padLeft(s, p string, count int) string {
+	ret := make([]byte, len(p)*count+len(s))
+
+	b := ret[:len(p)*count]
+	bp := copy(b, p)
+	for bp < len(b) {
+		copy(b[bp:], b[:bp])
+		bp *= 2
+	}
+	copy(ret[len(b):], s)
+	return string(ret)
+}
+
+func padRight(s, p string, count int) string {
+
+	return s + strings.Repeat(p, count)
+}
---
+++ pp/pretty-printer_test.go
@@ -0,0 +1,55 @@
+package pp
+
+import (
+	"testing"
+)
+
+func TestDump(t *testing.T) {
+
+	type someDumbStruct struct {
+		strVal string
+		intVal int
+		flVal  float64
+	}
+
+	s := someDumbStruct{
+		strVal: "blah blah blah",
+		intVal: 1337,
+		flVal:  42.0,
+	}
+
+	Dump(&s)
+
+	type anotherDumbStruct struct {
+		intVal int
+		vals   []int
+		strVal []someDumbStruct
+	}
+
+	strVals := []someDumbStruct{
+		{
+			strVal: "a",
+			intVal: 0,
+			flVal:  0,
+		},
+		{
+			strVal: "b",
+			intVal: 1,
+			flVal:  1,
+		},
+		{
+			strVal: "c",
+			intVal: 2,
+			flVal:  2,
+		},
+	}
+
+	a := anotherDumbStruct{
+		intVal: 6969,
+		vals:   []int{0, 1, 2, 3},
+		strVal: strVals,
+	}
+
+	Dump(&a)
+
+}
